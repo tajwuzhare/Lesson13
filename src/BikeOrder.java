6bbd1afb3af9cc53e1c1867357384c3c24cc415e
tux <tajwuzhare@yahoo.com>
2012-12-30 20:45:06 -0500

    classes created

---
+++ src/BikeOrder.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: heisenberg
+ * Date: 12/30/12
+ * Time: 8:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BikeOrder {
+	private JComboBox cbox;
+	private String[] models = {"BMX", "Huffy", "Murray", "Trek", "Schwinn"};
+
+	public static void main(String[] args){
+
+	}
+}
---
+++ src/BikeOrderEngine.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: heisenberg
+ * Date: 12/30/12
+ * Time: 8:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BikeOrderEngine implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	BikeOrderEngine(BikeOrder order){
+
+	}
+}
